0d3d656e9073eb86ca4469d382338332fe271cae
viniciustakedi <viniciustakedi7@gmail.com>
2021-04-01 14:24:17 -0300

    01-04 :robot:

--- AtividadesIgor-01-04/ProjetoReutilizavel/src/main/java/MediaDasIdades.java
+++ AtividadesIgor-01-04/ProjetoReutilizavel/src/main/java/MediaDasIdades.java
@@ -2,20 +2,25 @@ import java.util.Scanner;
 
 public class MediaDasIdades {
     public static void main(String[] args) {
-        //Par usar console
+        //Para usar console
         Scanner s = new Scanner(System.in);
 
         //Declaracão de variavel
-        int idade, soma, media;
+        String idade;
+        int num, media;
+        int soma = 0;
 
         //Coleta da idade
-        idade = s.nextInt();
-        
+        idade = s.next();
 
-        do {
-            
-        } while (idade > 0);
+        //Soma de todas as posicoes do numero
+        for (int i = 0; i < idade.length(); i++) {
+            num = Integer.parseInt(idade.substring(i,i+1));
+            soma = soma + num;
+        }
 
-
+        //media dos numeros
+        media = soma / idade.length();
+        System.out.println(media);
     }
 }
