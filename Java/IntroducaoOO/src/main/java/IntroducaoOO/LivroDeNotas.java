80824f6b4e8273c751646d4493abce1b22a77320
viniciustakedi <viniciustakedi7@gmail.com>
2021-04-09 20:09:45 -0300

    att 09-04

--- Java/IntroducaoOO/src/main/java/IntroducaoOO/LivroDeNotas.java
+++ Java/IntroducaoOO/src/main/java/IntroducaoOO/LivroDeNotas.java
@@ -1,11 +1,26 @@
 package IntroducaoOO;
 
 public class LivroDeNotas {
-    
-    String nomeDoCurso;
-    
-    public void exibirMensagem() {
-       System.out.println("Bem-vindo ao livro de notas!");
+
+    //Especificaros atributos
+    private String nomeDoCurso;
+
+    //Lista de metodos (acoes)
+    public void exibirMensagem(String nomeDoCurso) 
+    {
+        System.out.println("Bem-vindo ao livro de notas do curso " + nomeDoCurso);
+    }
+
+    //Metodo GET para o nomeDoCurso
+    public String getNomeDoCurso()
+    {
+        return nomeDoCurso;
     }
     
+    //Metodo SET para o nomeDoCurso
+    public void setNomeDoCurso(String nome)
+    {
+        this.nomeDoCurso = nome;
+    }
+
 }
--- Java/IntroducaoOO/src/main/java/IntroducaoOO/TesteLivroDeNotas.java
+++ Java/IntroducaoOO/src/main/java/IntroducaoOO/TesteLivroDeNotas.java
@@ -1,16 +1,26 @@
 package IntroducaoOO;
 
 public class TesteLivroDeNotas {
+
     public static void main(String[] args) {
-        
+
         //Declaracao do objeto livroDeNotas da classe LivroDeNotas
-        LivroDeNotas livroDeNotas;
+        //LivroDeNotas livroDeNotas;
         //Instanciando objeto livroDeNotas
-        livroDeNotas = new LivroDeNotas();
+        //livroDeNotas = new LivroDeNotas();
+        //Declaracao do objeto e instanciacao
+        LivroDeNotas livroA = new LivroDeNotas();
         
-        livroDeNotas.exibirMensagem();
+        livroA.exibirMensagem("P.O.O");
         
-        LivroDeNotas testeLivro = new LivroDeNotas();
-        testeLivro.exibirMensagem();
+        //SETando nome do curso
+        livroA.setNomeDoCurso("P.O.O");
+        
+        //GET pegando o nome do curso
+        System.out.println(livroA.getNomeDoCurso());
+        
+        LivroDeNotas livroB = new LivroDeNotas();
+        
+        livroB.exibirMensagem("D.B.");
     }
 }
